393599528b999fbcf8bcc7b3aa45d298f56c9cff
Benno Kohrs <bennokohrs@gmail.com>
2022-01-01 13:41:59 +0100

    fixed typo

--- client/src/ui/hooks/useGAEventsTracker.ts
+++
@@ -1,7 +0,0 @@
-import { event } from 'react-ga';
-
-export function useGAEventsTracker(category = 'unknown') {
-  return (action = 'unknown', label = 'unknown') => {
-    event({ category, action, label });
-  };
-}
